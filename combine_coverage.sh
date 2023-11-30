e2f194d5d9e160020e8d1c774a3130616a7379ca
maxim-lixakov <mvliksako1@mts.ru>
2023-11-30 19:01:22 +0300

    Revert "[DOP-9788] - initial commit"
    This reverts commit 35736d3f4b55584b2bee8e05808102e9ce88696b.

--- combine_coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-root_path=$(dirname $(realpath $0))
-coverage combine --rcfile=tests/.coveragerc
-coverage xml --rcfile=tests/.coveragerc -o $root_path/reports/coverage.xml -i
-coverage html --rcfile=tests/.coveragerc -d $root_path/reports/coverage.html -i
-coverage report --rcfile=tests/.coveragerc -i
--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-root_path=$(dirname $(realpath $0))
-$root_path/pytest_runner.sh "$@"
