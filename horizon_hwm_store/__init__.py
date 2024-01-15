f059d34fcbb5c3de07f7e9a1862beacf64592507
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-01-15 11:07:40 +0300

    [DOP-12026] Typing improvements

--- horizon_hwm_store/__init__.py
+++ horizon_hwm_store/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-FileCopyrightText: 2023-2024 MTS (Mobile Telesystems)
 # SPDX-License-Identifier: Apache-2.0
 from horizon_hwm_store.horizon_hwm_store import HorizonHWMStore
+
+__all__ = ["HorizonHWMStore"]
