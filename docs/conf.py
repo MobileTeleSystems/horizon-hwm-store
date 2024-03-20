423d37a4e24424bace5a96dea33938ccb2502578
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-03-20 12:02:39 +0300

    [DOP-13779] Add robots.txt to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -83,6 +83,7 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_extra_path = ["robots.txt"]
 favicons = [
     {"rel": "icon", "href": "icon.svg", "type": "image/svg+xml"},
 ]
